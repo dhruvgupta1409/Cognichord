2a21a0e3ce5370c871550a27da7aadb86f876ea8
Dhruv Gupta <dhruvgupta@Dhruvs-MacBook-Air.local>
2026-07-10 22:03:18 -0400

    Updates

---
+++ assets/book-open-CRdqkqgy.js
@@ -0,0 +1,11 @@
+import{c as a}from"./index-CrQulpH9.js";/**
+ * @license lucide-react v0.344.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=a("ArrowRight",[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"m12 5 7 7-7 7",key:"xquz4c"}]]);/**
+ * @license lucide-react v0.344.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const h=a("BookOpen",[["path",{d:"M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z",key:"vv98re"}],["path",{d:"M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z",key:"1cyq3y"}]]);export{e as A,h as B};
